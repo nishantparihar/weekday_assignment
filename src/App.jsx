195fbefb3ab6f6b84b72bb1340b907fa9e4dc1de
nishantparihar <nishprihar@gmail.com>
2024-05-06 00:59:08 +0530

    added axios call in MainBox.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import MainBox from './MainBox'
 
 function App() {
-
   return (
     <>
         <MainBox></MainBox>
--- src/MainBox.jsx
+++ src/MainBox.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from 'react';
 import FilterBox from './components/FilterBox';
 import JobsBox from './components/JobsBox';
 import Container from '@mui/material/Container'
-import { red } from '@mui/material/colors';
+import axios from 'axios';
+import { useRecoilState } from 'recoil';
+import { jdList } from './state/atoms/atoms';
 
 export default function MainBox(){
+
+    const [jobList, setJobList] = useRecoilState(jdList);
+
+    useEffect(()=>{
+        axios.post('https://api.weekday.technology/adhoc/getSampleJdJSON',{
+            "limit":10,
+            "offset":0
+        })
+        .then(res => {
+            setJobList(jobList => [...jobList, ...res.data.jdList])
+        });
+        
+    }, [])
+
     return <>
             <Container sx={{mt: 5}}>
                 <FilterBox></FilterBox>
