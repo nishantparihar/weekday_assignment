96455b9ed8001ab044ce2b1aff25d20a339d57ad
nishantparihar <nishprihar@gmail.com>
2024-05-06 01:39:10 +0530

    Unpdated CardAbout.jsx with fetched data

--- src/components/JobsBoxComponents/CardAbout.jsx
+++ src/components/JobsBoxComponents/CardAbout.jsx
@@ -16,7 +16,7 @@ const style = {
 };
 
 
-export default function CardAbout(){
+export default function CardAbout({jd}){
 
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
@@ -32,14 +32,7 @@ export default function CardAbout(){
             </Typography>
             <Typography sx={{ fontSize: '14px', color:'#000', height: '170px', overflow: 'hidden'}} >
                 <div>
-                    This is a sample job and you must have displayed it to understand that its 
-                    not just some random lorem ipsum text but something which was manually written. 
-                    Oh well, if random text is what you were looking for then here it is: Lorem Ipsum 
-                    is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
-                    the industry's
-                    Oh well, if random text is what you were looking for then here it is: Lorem Ipsum 
-                    is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
-                    the industry's
+                    {jd.jobDetailsFromCompany}
                 </div>
             </Typography>
 
@@ -59,15 +52,7 @@ export default function CardAbout(){
                             About us
                         </Typography>
                         <Typography sx={{ fontSize: '11px', color:'#000' }} >
-                            This is a sample job and you must have displayed it to understand that its 
-                            not just some random lorem ipsum text but something which was manually written. 
-                            Oh well, if random text is what you were looking for then here it is: Lorem Ipsum 
-                            is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
-                            the industry's standard dummy text ever since the 1500s, when an unknown printer took 
-                            a galley of type and scrambled it to make a type specimen book. It has survived not 
-                            only five centuries, but also the leap into electronic typesetting, remaining essentially 
-                            unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-                            Lorem Ipsum passages and now in this assignment.
+                            {jd.jobDetailsFromCompany}
                         </Typography>
                     </Box>
                 </Modal>
