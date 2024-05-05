66b5cd6763898da4a709cafe00e9371241c40011
nishantparihar <nishprihar@gmail.com>
2024-05-06 04:28:05 +0530

    Implemented Infinite scroll

--- src/MainBox.jsx
+++ src/MainBox.jsx
@@ -6,50 +6,56 @@ import axios from 'axios';
 import { useRecoilState } from 'recoil';
 import { jdList } from './state/atoms/atoms';
 
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-function clicked(){
-    axios.post('https://api.weekday.technology/adhoc/getSampleJdJSON',{
-        "limit":jobList.length + 6,
-        "offset":0
-    })
-    .then(res => {
-        const jdIds = jobList ? jobList.map(job => job.jdUid) : [];
-        const resJdList = res.data.jdList ? res.data.jdList.filter((job)=> !jdIds.includes(job.jdUid)) :[];
-        setJobList(jobList => [...jobList,...resJdList]);
-        
-    });
-}
 
 export default function MainBox(){
 
     const [jobList, setJobList] = useRecoilState(jdList);
 
 
-    function clicked(){
-        const jobListLength = jobList ? jobList.length : 0;
-        axios.post('https://api.weekday.technology/adhoc/getSampleJdJSON',{
-            "limit":jobListLength + 6,
-            "offset":0
-        })
-        .then(res => {
-            const jdIds = jobList ? jobList.map(job => job.jdUid) : [];
-            const resJdList = res.data.jdList ? res.data.jdList.filter((job)=> !jdIds.includes(job.jdUid)) :[];
-            setJobList(jobList => [...jobList,...resJdList]);
-            
-        });
+    function fetchData(){
+       try {
+            const jobListLength = jobList ? jobList.length : 0;
+            axios.post('https://api.weekday.technology/adhoc/getSampleJdJSON',{
+                "limit":jobListLength + 6,
+                "offset":0
+            })
+            .then(res => {
+                const jdIds = jobList ? jobList.map(job => job.jdUid) : [];
+                const resJdList = res.data.jdList ? res.data.jdList.filter((job)=> !jdIds.includes(job.jdUid)) :[];
+                setJobList(jobList => [...jobList,...resJdList]);
+                
+            })
+        } 
+        catch (error) {
+            console.log(error);
+        } 
+
     }
 
     useEffect(()=>{
-        clicked()
+        fetchData()
     }, [])
 
 
     return <>
-            <Container sx={{mt: 5}}>
-                <FilterBox></FilterBox>
-                <JobsBox></JobsBox>
-                <button onClick={clicked}>Load more</button>
-            </Container> 
-            
+        <Container sx={{mt: 5}}>
+            <FilterBox></FilterBox>
+            <InfiniteScroll
+                dataLength={jobList.length} //This is important field to render the next data
+                next={fetchData}
+                hasMore={jobList.length > 100 ? false : true}
+                loader={<h4>Loading...</h4>}
+                endMessage={
+                    <p style={{ textAlign: 'center' }}>
+                    <b>Yay! You have seen it all</b>
+                    </p>
+                }
+            >
+
+            <JobsBox></JobsBox>
+            </InfiniteScroll>
+        </Container>           
     </>
 }
