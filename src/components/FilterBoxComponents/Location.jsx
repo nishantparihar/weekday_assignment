4a10be15cd117bbc5432162ae0b90e9a3ec81f35
nishantparihar <nishprihar@gmail.com>
2024-05-06 13:57:06 +0530

    added recoil state in Location.jsx

--- src/components/FilterBoxComponents/Location.jsx
+++ src/components/FilterBoxComponents/Location.jsx
@@ -1,14 +1,18 @@
 import {Autocomplete, TextField} from '@mui/material';
-
+import { useSetRecoilState } from 'recoil';
+import { filterLocation } from '../../state/atoms/atoms';
 
 export default function Location(){
-    return <>
 
+    const setFilterLocation = useSetRecoilState(filterLocation);
+
+    return <>
         <Autocomplete
         multiple
         id="grouped-demo"
         size='small'
         options={locationList}
+        onChange={(e, v) => {setFilterLocation(v ? v.map((choice)=>choice.type) : [])}}
         sx={{ minWidth: 110}}
         blurOnSelect={true}            
         renderInput={(params) => <TextField {...params} label="Remote"/>}
@@ -20,8 +24,8 @@ export default function Location(){
 
 const locationList =[
 
-    {label: 'Remote', type: 1},
-    {label: 'Hybrid', type: 2},
-    {label: 'In-office', type: 3},
+    {label: 'Remote', type: "remote"},
+    {label: 'Hybrid', type: "hybrid"},
+    {label: 'In-office', type: "in-office"},
 
 ]
